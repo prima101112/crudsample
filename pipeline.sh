f5b62669ad059f186cffd541cdce3159e07f373f
prima101112 <prima101112@gmail.com>
2020-11-26 01:23:58 +0700

    try argo

--- pipeline.sh
+++ pipeline.sh
@@ -9,5 +9,4 @@ docker tag crudsample_backend prima101112/crudsample
 #push the image
 docker push prima101112/crudsample
 #deploy latest main
-
 argocd app create crudsample --repo /Users/prima/playground/crudsample-deployment/crudsample.git --path helm --dest-server https://192.168.64.3:8443 --dest-namespace crudsample
