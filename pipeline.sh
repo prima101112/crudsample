bd60d83c1deda9419a8479783913d5c8b6ba8560
prima101112 <prima101112@gmail.com>
2020-11-26 12:01:34 +0700

    add versioning

--- pipeline.sh
+++ pipeline.sh
@@ -5,9 +5,9 @@ env GOOS=linux GOARCH=amd64 go build -o ./bin/crudsample ./cmd/server/main.go
 # build docker image using compose
 docker-compose build --no-cache
 #tag the image
-docker tag crudsample_backend prima101112/crudsample
+docker tag crudsample_backend prima101112/crudsample:$1
 #push the image
-docker push prima101112/crudsample
+docker push prima101112/crudsample:$1
 #deploy latest main
 argocd app create crudsample --repo https://github.com/prima101112/crudsample.git --path helm --dest-server https://192.168.64.3:8443 --dest-namespace crudsample
 
