123d7700f4fff43fa63a564e84ab65a783e0e595
prima101112 <prima101112@gmail.com>
2020-11-26 13:13:22 +0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+#update helm image version in origin if there is ome repo this particular job is not needed
+sed -r -i '' 's/tag: ".*"/tag: "'$1'"/' /Users/prima/work/golang/src/github.com/prima101112/crudsample/helm/values.yaml
+git commit helm/values.yaml -m "update helm version to $1"
+#push to main origin to update so argodb could sync later in pipeline
+git push origin main
+
 printf "\n===== PRE : Create new branch from main =====\n\n"
 git checkout -b $1
 printf "\n===== ACTION : push to the branch version =====\n\n"
--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/google/uuid v1.1.2
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/lib/pq v1.0.0
+	github.com/lib/pq v1.8.0
 )
